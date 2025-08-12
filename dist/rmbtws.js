def173845a625fe90b4b318ec6e2314ad8f86272
Polina Gurina <76707268+polylina@users.noreply.github.com>
2025-08-12 17:29:52 +0200

    Build

--- dist/rmbtws.js
+++ dist/rmbtws.js
@@ -20,13 +20,6 @@
  *****************************************************************************!*/
 "use strict";
 
-/**
- * RMBTTest main object
- * @param {RMBTTestConfig} rmbtTestConfig
- * @param {RMBTControlServerCommunication} rmbtControlServer
- * @returns {}
- */
-
 Object.defineProperty(exports, "__esModule", {
     value: true
 });
@@ -35,7 +28,16 @@ var _createClass = function () { function defineProperties(target, props) { for 
 
 exports.RMBTTest = RMBTTest;
 
+var _geolocation = require("./geolocation");
+
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+/**
+ * RMBTTest main object
+ * @param {RMBTTestConfig} rmbtTestConfig
+ * @param {RMBTControlServerCommunication} rmbtControlServer
+ * @returns {}
+ */
 
 var MockLogger = function () {
     function MockLogger() {
@@ -212,6 +214,8 @@ function RMBTTest(rmbtTestConfig, rmbtControlServer) {
                         //only one thread will call after upload is finished
                         conductTest(response, thread, function () {
                             _logger.info("All tests finished");
+                            wsGeoTracker.stop();
+                            _rmbtTestResult.geoLocations = wsGeoTracker.getResults();
                             _rmbtTestResult.calculateAll();
                             _rmbtControlServer.submitResults(prepareResult(response), function () {
                                 setState(TestState.END);
@@ -235,7 +239,12 @@ function RMBTTest(rmbtTestConfig, rmbtControlServer) {
                 }
             };
 
-            continuation();
+            //get the user's geolocation
+            var wsGeoTracker = new _geolocation.GeoTracker();
+            _logger.debug("getting geolocation");
+            wsGeoTracker.start(function () {
+                continuation();
+            }, TestEnvironment.getTestVisualization());
         }, function () {
             //no internet connection
             callErrorCallback(RMBTError.REGISTRATION_FAILED);
@@ -1124,6 +1133,258 @@ function RMBTTest(rmbtTestConfig, rmbtControlServer) {
 
     construct(rmbtTestConfig, rmbtControlServer);
 };
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+var curGeoPos = void 0;
+var geo_callback = void 0,
+    loc_timeout = void 0;
+
+function runCallback() {
+    if (geo_callback != undefined && typeof geo_callback === 'function') {
+        window.setTimeout(function () {
+            geo_callback();
+        }, 1);
+    }
+}
+
+function getCurLocation() {
+    return curGeoPos;
+}
+
+/**
+ * GetLocation, JSDoc from old Test
+ * @param {Boolean} geoAccuracy enable high accuracy (i.e. GPS instead of AP)
+ * @param {Numeric} geoTimeout maximal timeout before error function is called
+ * @param {Numeric} geoMaxAge maximal allowed age in milliseconds
+ * @param {Function} callback
+ */
+function getLocation(geoAccuracy, geoTimeout, geoMaxAge, callback) {
+    var ausgabe = document.getElementById("infogeo");
+    geo_callback = callback;
+
+    if (!navigator.geolocation) {
+        //maybe there is a position in a cookie
+        //because the user had been asked for his address
+        var coords = getCookie('coords');
+        if (coords) {
+            var tmpcoords = JSON.parse(coords);
+            if (tmpcoords && tmpcoords['lat'] > 0 && tmpcoords['long'] > 0) {
+                testVisualization.setLocation(tmpcoords['lat'], tmpcoords['long']);
+            }
+        } else {
+            ausgabe.innerHTML = Lang.getString('NotSupported');
+        }
+
+        runCallback();
+        return;
+    }
+    runCallback();
+    //var TestEnvironment.getGeoTracker() = new GeoTracker();
+    TestEnvironment.getGeoTracker().start(function (successful, error) {
+        if (successful !== true) {
+            //user did not allow geolocation or other reason
+            if (error) {
+                switch (error.code) {
+                    case error.PERMISSION_DENIED:
+                        ausgabe.innerHTML = Lang.getString('NoPermission');
+                        break;
+                    case error.TIMEOUT:
+                        //@TODO: Position is determined...
+                        //alert(1);
+                        break;
+                    case error.POSITION_UNAVAILABLE:
+                        ausgabe.innerHTML = Lang.getString('NotAvailable');
+                        break;
+                    case error.UNKNOWN_ERROR:
+                        ausgabe.innerHTML = Lang.getString('NotAvailable') + "(" + error.code + ")";
+                        break;
+                }
+            } else {
+                //Internet Explorer 11 in some cases does not return an error code
+                ausgabe.innerHTML = Lang.getString('NotAvailable');
+            }
+        }
+    }, TestEnvironment.getTestVisualization());
+}
+
+//Geolocation tracking
+var GeoTracker = exports.GeoTracker = function () {
+    "use strict";
+
+    var _errorTimeout = 2e3; //2 seconds error timeout
+    var _maxAge = 60e3; //up to one minute old - don't do geoposition again
+
+    var _positions = void 0;
+    var _clientCallback = void 0;
+    var _testVisualization = null;
+
+    var _watcher = void 0;
+    var _firstPositionIsInAccurate = void 0;
+
+    function GeoTracker() {
+        _positions = [];
+        _firstPositionIsInAccurate = false;
+    }
+
+    /**
+     * Start geolocating
+     * @param {Function(Boolean)} callback expects param 'successful' (boolean, ErrorReason) and
+     *      is called as soon as there is a result available or the user cancelled
+     * @param {TestVisualization} testVisualization optional
+     */
+    GeoTracker.prototype.start = function (callback, testVisualization) {
+        _clientCallback = callback;
+
+        if (testVisualization !== undefined) {
+            _testVisualization = testVisualization;
+        }
+
+        if (navigator.geolocation) {
+            //try to get an rough first position
+            navigator.geolocation.getCurrentPosition(function (success) {
+                if (_positions.length === 0) {
+                    _firstPositionIsInAccurate = true;
+                    successFunction(success);
+                }
+            }, errorFunction, {
+                enableHighAccuracy: false,
+                timeout: _errorTimeout, //2 seconds
+                maximumAge: _maxAge //one minute
+            });
+            //and refine this position later
+            _watcher = navigator.geolocation.watchPosition(successFunction, errorFunction, {
+                enableHighAccuracy: true,
+                timeout: Infinity,
+                maximumAge: 0 //one minute
+            });
+        } else {
+            var t = _clientCallback;
+            _clientCallback = null;
+            t(false);
+        }
+
+        //Microsoft Edge does not adhere to the standard, and does not call the error
+        //function after the specified callback, so we have to call it manually
+        window.setTimeout(function () {
+            errorFunction();
+        }, _errorTimeout);
+    };
+
+    /**
+     * Saves the given result
+     * Is called when a geolocation query returns a result
+     * @param {Position} position the result https://developer.mozilla.org/en-US/docs/Web/API/Position
+     */
+    var successFunction = function successFunction(position) {
+        //rough first position and now more accurate one -> remove the inaccurate one
+        if (_positions.length === 1 && _firstPositionIsInAccurate) {
+            _positions = [];
+            _firstPositionIsInAccurate = false;
+        }
+
+        _positions.push({
+            geo_lat: position.coords.latitude,
+            geo_long: position.coords.longitude,
+            accuracy: position.coords.accuracy,
+            altitude: position.coords.altitude,
+            bearing: position.coords.heading,
+            speed: position.coords.speed,
+            tstamp: position.timestamp,
+            provider: 'Browser'
+        });
+        if (_clientCallback !== null) {
+            //call client that we now have a result
+            var t = _clientCallback;
+            _clientCallback = null;
+            t(true);
+        }
+        if (_testVisualization !== null) {
+            _testVisualization.setLocation(position.coords.latitude, position.coords.longitude);
+        }
+        updateCookie(position);
+    };
+
+    var errorFunction = function errorFunction(reason) {
+        //PositionError Object (https://developer.mozilla.org/en-US/docs/Web/API/PositionError)
+        if (_clientCallback !== null) {
+            //call client that we now have an error
+            var t = _clientCallback;
+            _clientCallback = null;
+            t(false, reason);
+        }
+    };
+
+    var updateCookie = function updateCookie(position) {
+        var coords = {};
+        coords['lat'] = position.coords.latitude;
+        coords['long'] = position.coords.longitude;
+        coords['accuracy'] = position.coords.accuracy;
+        coords['altitude'] = position.coords.altitude;
+        coords['heading'] = position.coords.heading;
+        coords['speed'] = position.coords.speed;
+        coords['tstamp'] = position.timestamp;
+        //console.log("coords: "+coords);
+        coords = JSON.stringify(coords);
+        //console.log("tmpcoords: "+tmpcoords);
+        setCookie('coords', coords, 3600);
+    };
+
+    GeoTracker.prototype.stop = function () {
+        if (navigator.geolocation) {
+            navigator.geolocation.clearWatch(_watcher);
+        }
+    };
+
+    /**
+     *
+     * @returns {Array} all results
+     */
+    GeoTracker.prototype.getResults = function () {
+        //filter duplicate results that can occur when using hardware GPS devices
+        //with certain Browsers
+        var previousItem = null;
+        _positions = _positions.filter(function (position) {
+            if (previousItem == null) {
+                previousItem = position;
+                return true;
+            }
+            var equal = Object.keys(position).every(function (key) {
+                return previousItem.hasOwnProperty(key) && previousItem[key] === position[key];
+            });
+            if (equal) {
+                //remove this item
+                return false;
+            } else {
+                previousItem = position;
+                return true;
+            }
+        });
+
+        return _positions;
+    };
+
+    return GeoTracker;
+}();
+
+/* getCookie polyfill */
+if (typeof window.setCookie === 'undefined') {
+    window.setCookie = function (cookie_name, cookie_value, cookie_exseconds) {
+        //var exdate = new Date();
+        //exdate.setDate(exdate.getDate() + cookie_exdays);
+
+        var futuredate = new Date();
+        var expdate = futuredate.getTime();
+        expdate += cookie_exseconds * 1000;
+        futuredate.setTime(expdate);
+
+        //var c_value=escape(cookie_value) + ((cookie_exdays==null) ? ";" : "; expires="+exdate.toUTCString() +";");
+        var c_value = encodeURIComponent(cookie_value) + (cookie_exseconds == null ? ";" : "; expires=" + futuredate.toUTCString() + ";");
+        document.cookie = cookie_name + "=" + c_value + " path=/;";
+    };
+}
 "use strict";
 
 /**
