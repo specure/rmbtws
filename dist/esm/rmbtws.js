214107b23bd5a9f765811f8353020fb1a19cbd73
Polina Gurina <76707268+polylina@users.noreply.github.com>
2025-12-12 10:30:24 +0100

    Log WebSocket error

--- dist/esm/rmbtws.js
+++ dist/esm/rmbtws.js
@@ -419,6 +419,7 @@ function RMBTTest(rmbtTestConfig, rmbtControlServer) {
     try {
       thread.socket = new WebSocket(server);
     } catch (e) {
+      _logger.error(e);
       callErrorCallback(RMBTError.SOCKET_INIT_FAILED);
       return;
     }
--- src/Websockettest.js
+++ src/Websockettest.js
@@ -457,6 +457,7 @@ export function RMBTTest(rmbtTestConfig, rmbtControlServer) {
         try {
             thread.socket = new WebSocket(server);
         } catch(e) {
+            _logger.error(e);
             callErrorCallback(RMBTError.SOCKET_INIT_FAILED);
             return;
         }
